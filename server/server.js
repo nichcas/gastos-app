d2c7f7fc429c1e70b4feb5a4fabb2e88033937a1
nichcas <nichcas@users.noreply.github.com>
2020-10-05 16:22:54 -0600

    Setup formating for amount and createdAt

--- server/server.js
+++ server/server.js
@@ -10,4 +10,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log(port);
+});
