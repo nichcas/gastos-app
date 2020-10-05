86d6a0206757f8e51b1a64e6e3c692b877067be3
nichcas <nichcas@users.noreply.github.com>
2020-10-05 14:30:19 -0600

    Setup production build ans server

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import { setTextFilter } from "./actions/filters";
-import getVisibleExpenses from "./selectors/expenses";
 
 import "./App.css";
 
